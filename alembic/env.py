12c286ce7b0e6f9222919fe3b4caf3e9fbc015e3
alexbaku <alexivl88@gmail.com>
2026-04-13 14:43:48 +0300

    fix: preserve app logging after alembic migration (disable_existing_loggers=False)

--- alembic/env.py
+++ alembic/env.py
@@ -24,7 +24,7 @@ config = context.config
 # This line sets up loggers basically.
 
 if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+    fileConfig(config.config_file_name, disable_existing_loggers=False)
 
 config.set_main_option("sqlalchemy.url", settings.database_url_sync)
 # add your model's MetaData object here
